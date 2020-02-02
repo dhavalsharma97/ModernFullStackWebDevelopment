04de18ec952e27810b4b8a07a7e5a0ea49228cd4
Dhaval Sharma <dhaval.sharma97@gmail.com>
2020-02-01 23:59:03 -0800

    Added Files

---
+++ public/app.js
@@ -0,0 +1,7 @@
+const element = React.createElement("div", null, React.createElement("h1", null, "Dhaval Sharma"), React.createElement("img", {
+  src: "Dhaval.jpg",
+  alt: "Head Shot"
+}), React.createElement("p", null, "I am Dhaval Sharma. I am currently pursuing masters in computer science from san diego state university. My hobbies are playing games and listening to music."), React.createElement("br", null), React.createElement("button", null, React.createElement("a", {
+  href: "https://github.com/dhavalsharma97/ModernFullStackWebDevelopment"
+}, "View My GitHub Repo")));
+ReactDOM.render(element, document.getElementById('content'));
---
+++ src/app.jsx
@@ -0,0 +1,11 @@
+const element = (
+    <div>
+      <h1>Dhaval Sharma</h1>
+      <img src = "Dhaval.jpg" alt = "Head Shot"></img>
+      <p>I am Dhaval Sharma. I am currently pursuing masters in computer science from san diego state university. My hobbies are playing games and listening to music.</p>
+      <br/>
+      <button><a href = "https://github.com/dhavalsharma97/ModernFullStackWebDevelopment">View My GitHub Repo</a></button>
+    </div>
+  );
+  
+  ReactDOM.render(element, document.getElementById('content'));
